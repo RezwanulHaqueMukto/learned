29a59b25190b9bd12307c1a2ea2899b6942716b2
REZWANUL1 <74784389+REZWANUL1@users.noreply.github.com>
2022-06-12 00:28:34 +0600

    adding mobile first menu

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,10 @@
 let nav_a = document.querySelectorAll("nav ul li a");
+let nav_links = document.querySelectorAll("nav ul li");
+let nav_container = document.querySelector(".nav-container");
+let nav_toggle = document.querySelector("#navToggle");
+
+//?#####NOTE:adding  active class in nav links at main navbar####
+//! #######################
 
 nav_a.forEach((link) => {
 	link.addEventListener("click", function () {
@@ -7,4 +13,16 @@ nav_a.forEach((link) => {
 		});
 		link.classList.add("active");
 	});
+});
+nav_links.forEach((element) => {
+	element.addEventListener("click", function () {
+		nav_container.classList.add("active");
+	});
+});
+
+//?#####NOTE:toggle active in nav-container for bring mobile menu####
+//! #######################
+
+nav_toggle.addEventListener("click", function () {
+	nav_container.classList.toggle("active");
 });
